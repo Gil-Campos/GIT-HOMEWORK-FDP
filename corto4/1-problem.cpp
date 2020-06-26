af59676a351f3ad5d2af2cb067f28e55682e3b7f
Gil Campos <47683202+Gil-Campos@users.noreply.github.com>
2020-06-26 02:36:54 -0600

    Add solution to the first problem of corto 4

---
+++ corto4/1-problem.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+void testArrays();
+void encriptData(char arr2[], int limit);
+
+void testArrays() {
+
+    int limit; 
+    char arr1[limit];
+
+    cout << endl;
+    cout << "Cuantos caracteres tiene tu palabra u oracion (incluyendo espacios): ";
+    cin >> limit;
+    cin.ignore(123, '\n');
+    cin.clear();
+    cout << endl;
+
+    cout << "tu palabra u oracion:" << endl;
+    /*
+    se le suma 1 al limite porque su declaracion equivale (int i = 0; i < limit - 1; i++), significando que i se queda un valor menos de las casillas 
+    del arreglo arr1[i], no pudiendo usar todas las casillas del arreglo que necesitamos para poder meter todos los caracteres.
+    */
+    cin.getline(arr1, limit + 1);   
+    cout << endl;
+
+    for (int i = 0; i < limit; i++) {
+        switch (arr1[i]) {
+        case 'm':
+            arr1[i] = '0';
+            break;
+        case 'u':
+            arr1[i] = '1';
+            break;
+        case 'r':
+            arr1[i] = '2';
+            break;
+        case 'c':
+            arr1[i] = '3';
+            break;
+        case 'i':
+            arr1[i] = '4';
+            break;
+        case 'e':
+            arr1[i] = '5';
+            break;
+        case 'l':
+            arr1[i] = '6';
+            break; 
+        case 'a':
+            arr1[i] = '7';
+            break;
+        case 'g':
+            arr1[i] = '8';
+            break;
+        case 'o':
+            arr1[i] = '9';
+            break;
+        default:
+            arr1[i] = arr1[i];
+            break;
+        }
+    }
+
+    for (int i = 0; i < limit; i++)
+    {
+        cout << arr1[i];
+    }
+      
+
+}
+
+int main() {
+    testArrays();
+    return 0;
+}
--- labo6/2-problem.cpp
+++ labo6/2-problem.cpp
@@ -6,13 +6,16 @@ int checkArray();
 
 int checkArray() {
 
+    //Arreglo dado
     int arr1[10] = {1,5, 6, 1, 1, 9, 8, 1, 3, 1};
+    //numero y contador para verificar cuantas veces se encuentra el entero en el arreglo
     int checkNum = 1, counter = 0;
 
     for (int i = 0; i < 10; i++)
     {
         if (arr1[i] == checkNum)
         {
+            //Se guarda la cantidad de veces que el numero se encuentra en el arreglo.
             counter += 1;
         }
         
