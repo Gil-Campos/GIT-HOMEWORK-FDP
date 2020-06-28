b863256ade823bb742687ed6a0c5bf977bf97bc0
Gil-Campos <47683202+Gil-Campos@users.noreply.github.com>
2020-06-27 19:13:11 -0600

    Update 3-problem.cpp

--- corto4/3-problem.cpp
+++ corto4/3-problem.cpp
@@ -60,7 +60,7 @@ void calcNota(float notasEstudiante[][5], int limit)
             cout << "El alumno " << i + 1 << " es una deshonra a su familia y a dejado la materia con una nota de " << sum << endl;
         }
         
-        sum = 0
+        sum = 0;
     } 
 
 }
