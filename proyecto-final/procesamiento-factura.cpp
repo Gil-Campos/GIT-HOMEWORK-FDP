fb412f93d96937bc392b9cd88938a9928da4f531
Gilberto Campos <47683202+Gil-Campos@users.noreply.github.com>
2020-07-13 12:33:34 -0600

    Finish final project with all the requirements, just doing some debugging later and comments

--- proyecto-final/procesamiento-factura.cpp
+++ proyecto-final/procesamiento-factura.cpp
@@ -15,17 +15,18 @@ struct costoPorArticulo
 };
 
 //Declaring functions
-void IncorporandoDatos(int size, costoPorArticulo []);
-void costoArticulos(int size, costoPorArticulo []);
+void IncorporandoDatos(int, costoPorArticulo []);
+void costoArticulos(int, costoPorArticulo []);
+void factura(int, costoPorArticulo []);
 
 void IncorporandoDatos(int size, costoPorArticulo arr2[]) 
 {
     string nombreArt;
 
+    cout << "---------------------------------------------" << endl;
     for (int i = 0; i < size; i++)
     {   
         cout << endl; 
-        cout << "---------------------------------------------" << endl;
         cout << "Nombre del producto " << i + 1 << ":" << endl;
         getline(cin, nombreArt, '\n');
         strncpy(arr2[i].nombreArticul, nombreArt.c_str(), longCad);
@@ -34,14 +35,37 @@ void IncorporandoDatos(int size, costoPorArticulo arr2[])
         cin >> arr2[i].cantidad;
         cout << "Precio del articulo:" << endl;
         cin >> arr2[i].costoPorArticulo;
-        cout << "---------------------------------------------" << endl;
         cin.ignore(123, '\n');
     }
+    cout << endl;
+    cout << "---------------------------------------------" << endl;
+}
+
+void costoArticulos(int size, costoPorArticulo arr3[]) 
+{
+    for (int i = 0; i < size; i++)
+    {
+        arr3[i].precio = (float)arr3[i].cantidad * arr3[i].costoPorArticulo;
+    }
+    
+}
+
+void factura(int size, costoPorArticulo arr4[])
+{
+    cout << endl;
+    cout << "Esta es la factura de tu compra" << endl;
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << "- " <<  arr4[i].cantidad << "x " << " " << arr4[i].nombreArticul << " $" << arr4[i].costoPorArticulo << endl;
+    }
+    
 }
 
 int main() {
 
     int cantiProdComprados;
+    float gasto = 0;
 
     cout << endl;
     cout << "Por favor ingresa la cantidad de articulos comprados:" << endl;
@@ -51,6 +75,17 @@ int main() {
     costoPorArticulo arr1[cantiProdComprados];
 
     IncorporandoDatos(cantiProdComprados, arr1);
+    costoArticulos(cantiProdComprados, arr1);
+    factura(cantiProdComprados, arr1);
+    
+    for (int i = 0; i < cantiProdComprados; i++)
+    {
+        gasto += arr1[i].precio;
+    }
+
+    cout << endl;
+    cout << "Total gastado $" << gasto;
+    cout << endl;
 
     return 0;
 }
